e0f1c128bed47f04862dc7e172bcb6e3470d512f
Richard <76806937+RixCrafts@users.noreply.github.com>
2025-06-12 20:20:12 -0400

    Update translation logic

--- js/custom-logic.js
+++ js/custom-logic.js
@@ -1,45 +1,38 @@
 
 document.addEventListener("DOMContentLoaded", function () {
+
+    // ------------------------ Contact Form Logic
     const checkbox = document.getElementById("checkbox-2");
     const fakeCheckbox = document.querySelector(".w-checkbox-input--inputType-custom");
     const checkboxLabel = document.querySelector(".checkbox-field");
     
     // Initilization
     fakeCheckbox.classList.remove("w--redirected-checked");
-    
-
-    console.log("Checkbox checked:", checkbox.checked);
-    console.log("Fake checkbox classList:", fakeCheckbox.classList);
-
 
     checkboxLabel.addEventListener("click", function () {
-        console.log("Checkbox label clicked");
         fakeCheckbox.classList.toggle("w--redirected-checked");
-
-        console.log("Fake checkbox classList:", fakeCheckbox.classList);
     });
-});
 
-// ...existing code...
-document.addEventListener('DOMContentLoaded', function () {
-    var langBtn = document.getElementById('lang-switch-btn');
-    var langLabel = document.querySelector('#lang-switch-label');
+
+    // ------------------------- Language Switch Logic
+    const langBtn = document.getElementById('lang-switch-btn');
+    const langLabel = document.querySelector('#lang-switch-btn-label');
 
     // Check if the current URL is the translated (Spanish) version
     const isSpanish = window.location.href.includes('_x_tr_tl=es');
 
-    langLabel.textContent = isSpanish ? 'EN' : 'ES';
+    langLabel.innerText = isSpanish ? 'ES' : 'EN';
 
     if (langBtn) {
         langBtn.addEventListener('click', function () {
-            if(!isSpanish){
+            if (!isSpanish) {
                 // Redirect to Spanish version (adjust URL as needed)
                 window.location.href = 'https://rixcrafts-github-io.translate.goog/TocadosLawFirm/?_x_tr_sl=en&_x_tr_tl=es&_x_tr_hl=en&_x_tr_pto=wapp';
-            }else{
+            } else {
                 // Redirect to English version (adjust URL as needed)
-                window.location.href = window.location.origin.replace('.translate.goog', '') || '/';
+                // window.location.href = window.location.origin.replace('.translate.goog', '') || '/';
+                window.location.href = 'https://rixcrafts-github-io/TocadosLawFirm/index.html';
             }
         });
     }
 });
-  // ...existing code...
